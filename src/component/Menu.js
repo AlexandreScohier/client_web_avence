1631299e3fc091c6188377d5d24690d969631724
AlexandreScohier <81409497+AlexandreScohier@users.noreply.github.com>
2021-12-22 17:02:31 +0100

    ajout du menu + style formulaire et menu

--- src/component/Menu.js
+++ src/component/Menu.js
@@ -6,7 +6,7 @@ class Menu extends React.Component{
     render() {
         return(
             <div className="menu">
-                <button>Modifier las mécaniciens</button>
+                <button>Modifier les mécaniciens</button>
                 <button>Modifier les garages</button>
                 <button>Modifier les disponibilités des mécaniciens</button>
                 <button>Modifier les réparations </button>
