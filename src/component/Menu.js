cb0ba4530c546c9cc23d05c5bd965c94bb8a91f6
Eragon6080 <matthysgaillard@gmail.com>
2021-12-22 16:49:37 +0100

    Ajout des composanyts aux menus

--- src/component/Menu.js
+++ src/component/Menu.js
@@ -1,18 +1,74 @@
 import React, {useState} from "react";
-import "../style/menuStyle.css"
-import "../style/mainStyle.css"
+import "../style/menuStyle.css";
+import "../style/mainStyle.css";
+import {
+    Button,
+    TextField,
+    Grid,
+    Paper,
+    Typography
+} from "@material-ui/core";
+import {Link, Redirect} from "react-router-dom";
+import {connect} from "react-redux";
 
 class Menu extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        }
+    }
     render() {
+        console.log("cc");
         return(
             <div className="menu">
-                <button>Modifier las mécaniciens</button>
-                <button>Modifier les garages</button>
-                <button>Modifier les disponibilités des mécaniciens</button>
-                <button>Modifier les réparations </button>
+                <Button
+                    variant={"contained"}
+                    label={"Modifier les garages"}
+                    onClick={()=>{
+                        return <Redirect to={"/garage"}/>}}>
+                    Modifier les garages
+                </Button>
+                <Button
+                    variant={"contained"}
+                    label={"Modifier les garages"}
+                    onClick={()=>{
+                        console.log("Essaie")
+                        return <Redirect to={"/garage"}/>}}>
+                    Modifier les mécaniciens
+                </Button>
+                <Button
+                    variant={"contained"}
+                    label={"Modifier les garages"}
+                    onClick={()=>{
+                        console.log("Essaie")
+                        return <Redirect to={"/garage"}/>}}>
+                    Modifier les horaires
+                </Button>
+                <Button
+                    variant={"contained"}
+                    label={"Modifier les garages"}
+                    onClick={()=>{
+                        console.log("Essaie")
+                        return <Redirect to={"/garage"}/>}}>
+                    Modifier les reparations
+                </Button>
             </div>
         )
     }
 }
+const mapStateToProps = (state)=>{
+    console.log("Matthys");
+    return {
+        user : state.userStore
+    }
+}
+const dispatchToProps = (dispatch)=>{
+    return {
+        login : (user)=>{
+            dispatch({type:"login", payload:{userInfo:user}})
+        }
+    }
+}
 
-export default Menu;
+export default connect(mapStateToProps,dispatchToProps)(Menu);
