e8963996f8200136e9779e7567e9e195ad3d499a
AlexandreScohier <81409497+AlexandreScohier@users.noreply.github.com>
2021-12-23 11:52:08 +0100

    Séléctionner un méchaniciens

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import './App.css';
 import Routes from "./routes/Routes";
-import SelectMechanics from "./component/SelectMechanics";
 import TableauGarage from "./component/Garage/GarageTable";
 function App() {
   return (
     <div className="App" >
-      <SelectMechanics/>
+      <TableauGarage/>
     </div>
   );
 }
