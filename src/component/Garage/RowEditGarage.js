c3107e487d5aac6bf167b70d2c4674277884437e
AlexandreScohier <81409497+AlexandreScohier@users.noreply.github.com>
2021-12-24 09:40:30 +0100

    transformation en elements

--- src/component/Garage/RowEditGarage.js
+++ src/component/Garage/RowEditGarage.js
@@ -5,6 +5,7 @@ import "../../style/tableStyle.css"
 const RowEditGarage = ({handleEditClick, garage}) =>{
     return(
         <tr>
+
             <td>
                 <input
                     type={"text"}
